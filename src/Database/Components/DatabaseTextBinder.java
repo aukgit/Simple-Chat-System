6edb67444357f8d59f5fd7c840553696e77afd68
Alim Ul Karim <auk.junk@live.com>
2014-12-04 02:59:39 +0600

    Build 04 Dec
    Build 04 Dec

--- src/Database/Components/DatabaseTextBinder.java
+++ src/Database/Components/DatabaseTextBinder.java
@@ -5,9 +5,9 @@
  */
 package Database.Components;
 
+import Database.DatabaseQuery;
 import java.util.ArrayList;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
 
 /**
@@ -15,16 +15,21 @@ import javax.swing.text.JTextComponent;
  * @author Alim
  */
 public class DatabaseTextBinder {
-    public DatabaseTextBinder(){
-    }
-    
-    public static void DbBindTextBoxes(JFrame frame, boolean read, boolean, insert, boolean update){
+
+
+    public static void DbBindTextBoxes(JFrame frame,DatabaseQuery db, boolean fieldNamesAppend, boolean read, boolean insert, boolean update) {
+        String[] fields =null;
         ArrayList<JTextComponent> list = JFrameRelatedCodes.getAllTextBox(frame);
         for (JTextComponent textBox : list) {
             //binding property name
             String accessName = textBox.getAccessibleContext().getAccessibleName();
-            
+
         }
-    
+
+        if (read) {
+            
+            db.setSpecialQueryFields_(fieldNamesAppend, fields);
+        }
+
     }
 }
