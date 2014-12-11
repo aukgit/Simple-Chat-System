fc1a748a735750ab67b6cff737f1c8714fecc48e
Alim Ul Karim <auk.junk@live.com>
2014-12-11 20:02:45 +0600

    Console many things added.
    Console many things added.

--- src/ConsolePackage/Console.java
+++ src/ConsolePackage/Console.java
@@ -8,6 +8,7 @@ package ConsolePackage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  *
@@ -15,9 +16,16 @@ import java.io.InputStreamReader;
  */
 public class Console {
 
-    
     public static void writeLine(Object o) {
         System.err.println(o);
+    }
+
+    public static void writeLine(char[] array) {
+        System.err.println(array);
+    }
+
+    public static void writeLine(String s) {
+        System.err.println(s);
     }
 
     public static void write(Object o) {
@@ -28,25 +36,44 @@ public class Console {
         try {
             BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
             return bufferRead.readLine();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-    
-    public static String readInt() {
-        try {
-            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+
+    /**
+     * Scanner can do everything.
+     *
+     * @return
+     */
+    public static Scanner getScanner() {
+        return new Scanner(System.in);
     }
-    
+
+    public static float readFloat() {
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextFloat();
+
+    }
+
+    public static double readDouble() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+
+    public static int readInt() {
+
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+
+    }
+
     public static char readKey() {
         String str = readLine();
-        if(str != null && str.length() > 0){
+        if (str != null && str.length() > 0) {
             return str.charAt(0);
         }
         return '\0';
